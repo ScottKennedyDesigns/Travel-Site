4d2aaae4f0c1acd31960da68488502283b9423e9
scottkennedydesigns <scott@scottkennedydesigns.com>
2019-08-26 00:52:41 +0300

    Testing the gulpfile, as before dealing with a lot errors before hand

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-const gulp = reuire('gulp')
+const gulp = require('gulp')
 
 gulp.task('default', function() {
-    
+    console.log('Yay, you did Gulp!')
+})
+
+gulp.task('html', function() {
+    console.log("Imagine something useful being done to your html.")
 })
